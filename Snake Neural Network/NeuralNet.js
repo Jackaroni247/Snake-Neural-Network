4d58e1f572645b9284d53d0647efa971c2a55274
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2023-06-01 22:31:12 -0500

    Update NeuralNet.js

--- Snake Neural Network/NeuralNet.js
+++ Snake Neural Network/NeuralNet.js
@@ -24,23 +24,35 @@ class neuralNetwork {
   weights = [];
 
   constructor() {
-      for(var i = 0; i < this.amountHiddenLayers + 2; i++) {
-        this.layers[i] = [];
-        if (i === 0) {
-          for (var j = 0; j < this.inputLayerSize; j++) {
-            this.layers[i][j] = 0;
-          }
-        } else if (i === this.amountHiddenLayers + 1) {
-          for (var j = 0; j < this.outputLayerSize; j++) {
-            this.layers[i][j] = 0;
-          }
-        } else {
-          for (var j = 0; j < this.hiddenLayerSize; j++) {
-            this.layers[i][j] = 0;
-          }
+    //  Setting Up The Arrays Of Weights And Nodes
+    for (var i = 0; i < this.amountHiddenLayers + 2; i++) {
+      this.layers[i] = [];
+      if (i === 0) {
+        for (var j = 0; j < this.inputLayerSize; j++) {
+          this.layers[i][j] = 0;
+        }
+      } else if (i === this.amountHiddenLayers + 1) {
+        for (var j = 0; j < this.outputLayerSize; j++) {
+          this.layers[i][j] = 0;
+        }
+      } else {
+        for (var j = 0; j < this.hiddenLayerSize; j++) {
+          this.layers[i][j] = 0;
         }
       }
-      console.log(this.layers);
+    }
+    for (var i = 1; i < this.layers.length; i++) {
+      this.weights[i] = [];
+      for (var j = 0; j < this.layers[i].length; j++) {
+        this.weights[i][j] = [];
+        for (var k = 0; k < this.layers[i - 1].length; k++) {
+          this.weights[i][j][k] = 0;
+        }
+      }
+    }
+
+    console.log(this.layers);
+    console.log(this.weights);
   }
 
   inForOut(
@@ -55,7 +67,7 @@ class neuralNetwork {
     xDistToNearestSnake,
     yDistToNearestSnake
   ) {
-    this.inputLayer = [
+    this.layers[0] = [
       xDistToApple,
       yDistToApple,
       xDir,
@@ -67,8 +79,18 @@ class neuralNetwork {
       xDistToNearestSnake,
       yDistToNearestSnake,
     ];
+    var sum = 0;
+    for (var i = 1; i < this.weights.length; i++) {
+      for (var j = 0; j < this.weights[i].length; j++) {
+        sum = 0;
+        for (var k = 0; k > this.weights[i][j].length; l++) {
+          sum += this.weights[i][j][k] * this.layers[i - 1][k];
+        }
+        this.layer[i][j] = sum;
+      }
+    }
 
-    this.layers[this.layers.length-1] = [0.25, 0.25, 0.25, 0.25];
+    this.layers[this.layers.length - 1] = [0.25, 0.25, 0.25, 0.25];
   }
 }
 
