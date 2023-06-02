dc52f34d42310b26676c5ef5f5a0145b38bafe7b
Jackaroni <121079788+Jackaroni247@users.noreply.github.com>
2023-06-02 11:42:10 -0500

    Update NeuralNet.js

--- Snake Neural Network/NeuralNet.js
+++ Snake Neural Network/NeuralNet.js
@@ -14,6 +14,7 @@
 // Y DIST TO NEAREST WALL
 
 class neuralNetwork {
+  e = 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274;
   inputLayerSize = 10;
   amountHiddenLayers = 2;
   hiddenLayerSize = 15;
@@ -46,11 +47,10 @@ class neuralNetwork {
       for (var j = 0; j < this.layers[i].length; j++) {
         this.weights[i][j] = [];
         for (var k = 0; k < this.layers[i - 1].length; k++) {
-          this.weights[i][j][k] = 0;
+          this.weights[i][j][k] = random(0, 5);
         }
       }
     }
-
     console.log(this.layers);
     console.log(this.weights);
   }
@@ -83,15 +83,24 @@ class neuralNetwork {
     for (var i = 1; i < this.weights.length; i++) {
       for (var j = 0; j < this.weights[i].length; j++) {
         sum = 0;
-        for (var k = 0; k > this.weights[i][j].length; l++) {
-          sum += this.weights[i][j][k] * this.layers[i - 1][k];
+        for (var k = 0; k < this.weights[i][j].length; k++) {
+          sum += this.weights[i][j][k] * this.layers[i - 1][j];
         }
-        this.layer[i][j] = sum;
+        this.layers[i][j] = 1 / (1 + pow(this.e, 0 - sum));
       }
     }
-
-    this.layers[this.layers.length - 1] = [0.25, 0.25, 0.25, 0.25];
+    console.log(this.layers);
+    sum = 0;
+    for (var i = 0; i < this.layers[this.layers.length - 1].length; i++) {
+      sum += this.layers[this.layers.length - 1][i];
+    }
+    for (var i = 0; i < this.layers[this.layers.length - 1].length; i++) {
+      this.layers[this.layers.length - 1][i] =
+        this.layers[this.layers.length - 1][i] / sum;
+    }
   }
 }
 
-function createChildNet() {}
+function createChildNet() {
+  // The Best Of The Generation Will Have Ten Children With Slight Mutations
+}
